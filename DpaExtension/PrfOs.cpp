a4c48653c25c942fbc042227a6b3be46903abb16
Frantisek Mikulu <frantisek.mikulu@openhs.com>
2017-02-05 17:03:34 +0100

    Implemented PrfOs command SLEEP

---
+++ DpaExtension/PrfOs.cpp
@@ -0,0 +1,131 @@
+#include "PrfOs.h"
+#include "IqrfLogging.h"
+
+const std::string PrfOs::PRF_NAME("OS");
+
+// Commands
+const std::string STR_CMD_READ("READ");
+const std::string STR_CMD_RESET("RESET");
+const std::string STR_CMD_READ_CFG("READ_CFG");
+const std::string STR_CMD_RFPGM("RFPGM");
+const std::string STR_CMD_SLEEP("SLEEP");
+const std::string STR_CMD_BATCH("BATCH");
+const std::string STR_CMD_SET_USEC("SET_USEC");
+const std::string STR_CMD_SET_MID("SET_MID");
+const std::string STR_CMD_RESTART("RESTART");
+const std::string STR_CMD_WRITE_CFG_BYTE("WRITE_CFG_BYTE");
+const std::string STR_CMD_LOAD_CODE("LOAD_CODE");
+const std::string STR_CMD_WRITE_CFG("WRITE_CFG");
+
+using namespace std::chrono;
+
+PrfOs::PrfOs()
+  :DpaTask(PRF_NAME, PNUM_OS)
+{
+}
+
+PrfOs::PrfOs(uint16_t address)
+  : DpaTask(PRF_NAME, PNUM_OS, address, (uint8_t)Cmd::READ)
+{
+}
+
+PrfOs::~PrfOs()
+{
+}
+
+void PrfOs::parseResponse(const DpaMessage& response)
+{
+  //TODO
+  //m_8Temperature = response.DpaPacket().DpaResponsePacket_t.DpaMessage.PerThermometerRead_Response.IntegerValue;
+}
+
+void PrfOs::sleep(const std::chrono::seconds& sec, uint8_t ctrl)
+{
+  setCmd(Cmd::SLEEP);
+  ctrl &= 0x0F; //reset milis flag
+  milis2097 ms2097 = duration_cast<milis2097>(sec);
+  uint16_t tm = (uint16_t)ms2097.count();
+  m_request.DpaPacket().DpaRequestPacket_t.DpaMessage.PerOSSleep_Request.Time = tm;
+  m_request.DpaPacket().DpaRequestPacket_t.DpaMessage.PerOSSleep_Request.Control = ctrl;
+  m_request.SetLength(sizeof(TDpaIFaceHeader) + 3);
+}
+
+void PrfOs::sleep(const std::chrono::milliseconds& milis, uint8_t ctrl)
+{
+  setCmd(Cmd::SLEEP);
+  ctrl &= 0x0F; //reset other flags
+  ctrl |= 0x10; //set milis flags
+  micros32768 mc32768 = duration_cast<micros32768>(milis);
+  uint16_t tm = (uint16_t)mc32768.count();
+  m_request.DpaPacket().DpaRequestPacket_t.DpaMessage.PerOSSleep_Request.Time = tm;
+  m_request.DpaPacket().DpaRequestPacket_t.DpaMessage.PerOSSleep_Request.Control = ctrl;
+  m_request.SetLength(sizeof(TDpaIFaceHeader) + 3);
+}
+
+void PrfOs::calibration()
+{
+  setCmd(Cmd::SLEEP);
+  m_request.DpaPacket().DpaRequestPacket_t.DpaMessage.PerOSSleep_Request.Time = 0;
+  m_request.DpaPacket().DpaRequestPacket_t.DpaMessage.PerOSSleep_Request.Control = (uint8_t)TimeControl::RUN_CALIB;
+}
+
+PrfOs::Cmd PrfOs::getCmd() const
+{
+  return m_cmd;
+}
+
+void PrfOs::setCmd(PrfOs::Cmd cmd)
+{
+  m_cmd = cmd;
+  setPcmd((uint8_t)m_cmd);
+}
+
+void PrfOs::parseCommand(const std::string& command)
+{
+  if (STR_CMD_READ == command)
+    setCmd(Cmd::READ);
+  else if (STR_CMD_RESET == command)
+    setCmd(Cmd::RESET);
+  else if (STR_CMD_READ_CFG == command)
+    setCmd(Cmd::READ_CFG);
+  else if (STR_CMD_RFPGM == command)
+    setCmd(Cmd::RFPGM);
+  else if (STR_CMD_SLEEP == command)
+    setCmd(Cmd::SLEEP);
+  else if (STR_CMD_BATCH == command)
+    setCmd(Cmd::BATCH);
+  else if (STR_CMD_SET_USEC == command)
+    setCmd(Cmd::SET_USEC);
+  else if (STR_CMD_SET_MID == command)
+    setCmd(Cmd::SET_MID);
+  else if (STR_CMD_RESTART == command)
+    setCmd(Cmd::RESTART);
+  else if (STR_CMD_WRITE_CFG_BYTE == command)
+    setCmd(Cmd::WRITE_CFG_BYTE);
+  else if (STR_CMD_LOAD_CODE == command)
+    setCmd(Cmd::LOAD_CODE);
+  else if (STR_CMD_WRITE_CFG == command)
+    setCmd(Cmd::WRITE_CFG);
+  else
+    THROW_EX(std::logic_error, "Invalid command: " << PAR(command));
+}
+
+const std::string& PrfOs::encodeCommand() const
+{
+  switch (getCmd()) {
+  case Cmd::READ: return STR_CMD_READ;
+  case Cmd::RESET: return STR_CMD_RESET;
+  case Cmd::RFPGM: return STR_CMD_RFPGM;
+  case Cmd::SLEEP: return STR_CMD_SLEEP;
+  case Cmd::BATCH: return STR_CMD_BATCH;
+  case Cmd::SET_USEC: return STR_CMD_SET_USEC;
+  case Cmd::SET_MID: return STR_CMD_SET_MID;
+  case Cmd::RESTART: return STR_CMD_RESTART;
+  case Cmd::WRITE_CFG_BYTE: return STR_CMD_WRITE_CFG_BYTE;
+  case Cmd::LOAD_CODE: return STR_CMD_LOAD_CODE;
+  case Cmd::WRITE_CFG: return STR_CMD_WRITE_CFG;
+
+  default:
+    THROW_EX(std::logic_error, "Invalid command: " << NAME_PAR(command, (int)getCmd()));
+  }
+}
---
+++ DpaExtension/PrfOs.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "DpaTask.h"
+#include <chrono>
+
+class PrfOs : public DpaTask
+{
+public:
+  enum class Cmd {
+    READ = CMD_OS_READ,
+    RESET = CMD_OS_RESET,
+    READ_CFG = CMD_OS_READ_CFG,
+    RFPGM = CMD_OS_RFPGM,
+    SLEEP = CMD_OS_SLEEP,
+    BATCH = CMD_OS_BATCH,
+    SET_USEC = CMD_OS_SET_USEC,
+    SET_MID = CMD_OS_SET_MID,
+    RESTART = CMD_OS_RESTART,
+    WRITE_CFG_BYTE = CMD_OS_WRITE_CFG_BYTE,
+    LOAD_CODE = CMD_OS_LOAD_CODE,
+    WRITE_CFG = CMD_OS_WRITE_CFG
+  };
+
+  enum class TimeControl {
+    WAKEUP_PB4_NEGATIVE = 1,
+    RUN_CALIB = 2,
+    LEDG_FLASH = 4,
+    WAKEUP_PB4_POSITIVE = 8
+  };
+
+  static const std::string PRF_NAME;
+
+  PrfOs();
+  PrfOs(uint16_t address);
+  virtual ~PrfOs();
+
+  void parseResponse(const DpaMessage& response) override;
+  void parseCommand(const std::string& command) override;
+  const std::string& encodeCommand() const override;
+
+  void sleep(const std::chrono::seconds& sec, uint8_t ctrl = 0);
+  void sleep(const std::chrono::milliseconds& milis, uint8_t ctrl = 0);
+  void calibration();
+
+private:
+  typedef std::chrono::duration<unsigned long, std::ratio<2097, 1000>> milis2097;
+  typedef std::chrono::duration<unsigned long, std::ratio<32768, 1000000>> micros32768;
+
+  Cmd getCmd() const;
+  void setCmd(Cmd cmd);
+  Cmd m_cmd = Cmd::READ;
+
+  uint16_t m_time = 0;
+  uint8_t m_timeCtrl = 0;
+};
