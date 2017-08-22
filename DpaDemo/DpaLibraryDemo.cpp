96a53ad96c2a2e1fc19d5f1ee2daf18afb44c6c7
Rostislav Spinar <rostislav.spinar@microrisc.com>
2017-08-22 13:40:40 +0200

    Reflect changes from DpaRequest to DpaTransfer in demo

--- DpaDemo/DpaLibraryDemo.cpp
+++ DpaDemo/DpaLibraryDemo.cpp
@@ -238,9 +238,9 @@ void DpaLibraryDemo::readTemperature(uint16_t address) {
   executeCommand(message);
 
   // process the response
-  if (m_dpaHandler->Status() == DpaRequest::DpaRequestStatus::kProcessed) {
+  if (m_dpaHandler->Status() == DpaTransfer::DpaTransferStatus::kProcessed) {
     int16_t temperature =
-      m_dpaHandler->CurrentRequest().ResponseMessage().DpaPacket().DpaResponsePacket_t.DpaMessage.PerThermometerRead_Response.IntegerValue;
+      m_dpaHandler->CurrentTransfer().ResponseMessage().DpaPacket().DpaResponsePacket_t.DpaMessage.PerThermometerRead_Response.IntegerValue;
 
     std::cout << num++ << " Temperature: "
       << std::dec << temperature << " oC" << std::endl;
@@ -270,7 +270,7 @@ void DpaLibraryDemo::executeCommand(DpaMessage& message) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
-  if (m_dpaHandler->Status() == DpaRequest::DpaRequestStatus::kTimeout) {
+  if (m_dpaHandler->Status() == DpaTransfer::DpaTransferStatus::kTimeout) {
     ++timeouts;
     std::cout << message.NodeAddress()
       << " - Timeout ..."
